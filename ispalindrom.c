d7ff4acdb932aa1508a1beea96975cdef2b11ef0
Thomas Stimakovits <thomas@kodas.io>
2020-11-14 18:35:26 +0100

    username changed

--- ispalindrom.c
+++ ispalindrom.c
@@ -22,6 +22,7 @@ int main (int argc, char *argv[]){
     isPalindrom("abba");
     isPalindrom("lagerregal");
     isPalindrom("anna abba anna");
+    isPalindrom("Sanna abba annas");
     return 0;
 }
 
