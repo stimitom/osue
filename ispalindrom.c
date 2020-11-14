b8936b8ec3654e9ed85e7440dab80a35fa40cdce
Thomas Stimakovits <thomas@kodas.io>
2020-11-14 20:23:54 +0100

    Reads from stdin

--- ispalindrom.c
+++ ispalindrom.c
@@ -1,28 +1,42 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-void isPalindrom(char str[]) {
-    int l = 0; 
-    int h = strlen(str) -1; 
+void isPalindrom(char str[])
+{
+    int l = 0;
+    int h = strlen(str) - 1;
 
-
-    while (h > l) {
+    while (h > l)
+    {
         if (str[l++] != str[h--])
         {
             /* Compares characters from beginning and end to the middle. Returns if they are not equal.*/
-            printf("%s is not a palindrom.\n", str); 
+            printf("%s is not a palindrom.\n", str);
             return;
         }
     }
     printf("%s is a palindrom.\n", str);
 }
 
-int main (int argc, char *argv[]){
-    isPalindrom("Abba"); 
-    isPalindrom("abba");
-    isPalindrom("lagerregal");
-    isPalindrom("anna abba anna");
-    isPalindrom("Sanna abba annas");
+int main(int argc, char *argv[])
+{
+    char *input = NULL;
+    size_t len = 0;
+    while (!feof(stdin))
+    {
+        ssize_t linesize = getline(&input, &len, stdin);
+        if (input[linesize - 1] == '\n')
+        {
+            // removes newline character from input string
+            input[linesize - 1] = '\0';
+            --linesize;
+        }
+
+        isPalindrom(input);
+    }
+
+    free(input);
+
     return 0;
 }
-
